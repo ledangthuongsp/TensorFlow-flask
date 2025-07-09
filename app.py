025dc6bce3c6543841b5926140984bcefb1ef742
IDS-V-Thuongld <thuongld@vn.ids.jp>
2025-07-10 00:13:46 +0700

    save

--- app.py
+++ app.py
@@ -66,5 +66,4 @@ def detect_waste_type():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    app.run()
